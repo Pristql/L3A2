c9c01da4129862a1fd44fa8e909e70b02bfac927
Pristql <luigimansion111@gmail.com>
2026-04-15 18:00:30 +0530

    Add files via upload

---
+++ L3A2.py
@@ -0,0 +1,24 @@
+name="Soham"
+age=12
+height=5.1
+is_student=True
+
+print("Name: ", name)
+print("Data type of name is:", type(name))
+
+print("Age: ", age)
+print("Data type of age is:", type(age))
+
+print("Height: ", height)
+print("Data type of height is:", type(height))
+
+print("is student: ", is_student)
+print("Data type of is_student is:", type(is_student))
+
+age=str(age)
+print(age)
+print("Data type of age is: ", type(age))
+
+height=int(height)
+print(height)
+print("Data type of height is: ", type(height))
